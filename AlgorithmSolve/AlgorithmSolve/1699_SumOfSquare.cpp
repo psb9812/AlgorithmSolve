87cae3d71580f13a8914f075f529f662fea77e3c
tjdqls070 <tjdqls070@naver.com>
2024-06-23 00:43:48 +0900

    4주차 DP 문제 풀이

---
+++ AlgorithmSolve/AlgorithmSolve/1699_SumOfSquare.cpp
@@ -0,0 +1,38 @@
+/*
+백준 1699번
+	제곱수의 합
+
+풀이:
+
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int D[100001];
+int n;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+
+	D[0] = 0;
+	D[1] = 1;
+
+	for (int k = 2; k <= n; k++)
+	{
+		D[k] = k;
+		for (int j = 1; j * j <= k; j++)
+		{
+			D[k] = min(D[k] , 1 + D[k - (j * j)]);
+		}
+	}
+
+	cout << D[n];
+}
